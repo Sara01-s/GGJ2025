b7d855f82e9fd06b704278cd3503ec9a6a89a617
PelucaP <tomas.carcamo.g@usach.cl>
2025-01-22 13:51:28 -0300

    Se añade lo basico del timer de cliente y la actualizacion del mismo para ser utilizado cuando aumentará la dificultad

---
+++ slasherbubbletea/timer.gd
@@ -0,0 +1,14 @@
+extends Timer
+
+signal aumentarDificultad
+@onready var timer: Timer = $"."
+@onready var current_timer = 10
+
+func _on_timeout() -> void:
+	print("Se acabo el tiempo")
+	aumentarDificultad.emit()
+
+func _on_aumentar_dificultad() -> void:
+	print("\nEstamos acá")
+	timer.wait_time = current_timer
+	timer.start()
